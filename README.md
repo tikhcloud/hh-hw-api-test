dfb308b089de494e047da758c41a111e0a8659a4
Eduard Tikhomirov <tikhomirovcloud@icloud.com>
2020-12-16 22:05:14 +0300

    Add tests

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## hh-hw-api-test
+**To run tests**
+
+    git clone https://github.com/tikhcloud/hh-hw-api-test.git
+    cd hh-hw-api-test
+    
+**Insert HH auth token and run `mvn clean test`**
+
+    mvn clean -Dhh_auth=<TOKEN> test
--- src/test/java/TestApiVacancyText.java
+++ src/test/java/TestApiVacancyText.java
@@ -39,4 +39,147 @@ public class TestApiVacancyText {
                     .spec(checkStatusCodeAndContentType);
     }
 
+    @Test
+    public void testPutRequest() {
+        given()
+                .when()
+                    .put()
+                .then()
+                    .statusCode(405);
+    }
+
+    @Test
+    public void testDeleteRequest() {
+        given()
+                .when()
+                    .delete()
+                .then()
+                    .statusCode(405);
+    }
+
+    @Test
+    public void testEmptyRequest() {
+        given()
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testOneCharRequest() {
+        given().param("text", "j")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testEngSimpleRequest() {
+        given().param("text", "java")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testQuotesRequest() {
+        given().param("text", "\"java\"")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testTwoEngRequest() {
+        given().param("text", "Java Developer")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testCyrillicSimpleRequest() {
+        given().param("text", "Джава")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testTwoCyrillicRequest() {
+        given().param("text", "Джава разработчик")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testNumericSimpleRequest() {
+        given().param("text", "1929391293919")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testNumericEngRequest() {
+        given().param("text", "1k3kj12233jjj kk3j3k 222")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testNumericCyrillicRequest() {
+        given().param("text", "лдыофа212оов цо221329влй 122")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testSymbolsRequest() {
+        given().param("text", " ßåƒ ƒ˜˜Ωç√˜˜åœ∑´´††¥¨ˆˆπ“«æ…¬˚∆˙©ƒ√∫˜˜≤≥ç˚£•™ª•¢¶ ƒ¬å…µ√Ωçæ«……“‘œ“")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testOrRequest() {
+        given().param("text", "Java OR Swift")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testAndRequest() {
+        given().param("text", "Java AND Swift")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
+
+    @Test
+    public void testNotRequest() {
+        given().param("text", "Java NOT Python")
+                .when()
+                    .get()
+                .then()
+                    .spec(checkStatusCodeAndContentType);
+    }
 }
